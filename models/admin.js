e5def87ba18c9aa54d32aa722cbe48b3866e42b6
Ali Hamza Urf PP <hamza120025@gmail.com>
2022-04-20 10:10:51 +0500

    implemented all models for db

--- models/admin.js
+++ models/admin.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const teacherSchema = new Schema({
+const adminSchema = new Schema({
 	name: {
 		type: String,
 		required: true,
 	},
-	designation: {
+	rollNo: {
 		type: String,
 		required: true,
 	},
 });
 
-module.exports = mongoose.model("Teacher", teacherSchema);
+module.exports = mongoose.model("Admin", adminSchema);
--- models/student.js
+++ models/student.js
@@ -1,14 +1,15 @@
-var mongoose = require("mongoose")
-var Schema = mongoose.Schema
-var studentSchema=new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    rollno:{
-        type:String,
-        required:true
-    }
-})
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-module.exports=mongoose.model('Student',studentSchema)
+const studentSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	rollNo: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("Student", studentSchema);
